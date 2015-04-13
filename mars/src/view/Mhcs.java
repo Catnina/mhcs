12954e6f108dfa04bb29994c3f3ef34697d232c4
jaymadden <maddenj@d.umn.edu>
2015-04-13 17:49:36 -0500

    worked in gui

---
+++ mars/src/view/Mhcs.java
@@ -0,0 +1,15 @@
+package view;
+
+/**
+ * creates the graphical user interface. 
+ * Event Bus handles adding and removing modules. 
+ * 
+ * @author Janna Madden
+ *
+ */
+
+
+public class Mhcs{
+
+	
+	  }
